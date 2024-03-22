b936164244fd098ec06949c09b635aa3bd45b8e2
Amanda Arndt <162518749+amandaarndt@users.noreply.github.com>
2024-03-22 18:20:37 -0300

    FirstC.c

--- FirstC.c
+++ FirstC.c
@@ -2,4 +2,6 @@ main () {
   printf("Relembrando C \n");
   printf("Este é o número dois: %d\n", 2);
   printf("%s está a %d milhões de milhas do sol\n","Venus",67);
+  int num = 2;
+  printf("O número %d é par\n",num);
 }
