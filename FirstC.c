4633025e501053a2a7a725b7e631c11a51f9e96c
Amanda Arndt <162518749+amandaarndt@users.noreply.github.com>
2024-03-24 16:15:00 -0300

    FirstC.c

--- FirstC.c
+++ FirstC.c
@@ -1,7 +1,7 @@
 main () {
   printf("Relembrando C \n");
   printf("Este é o número dois: %d\n", 2);
+  int num = 2;
   printf("O número %d é par\n",num);
   printf("%s está a %d milhões de milhas do sol\n","Venus",67);
-  int num = 2;
 }
